319296079fb605f7f5c9fef75edadb457a851ee6
Ibrahim Hz <mohammedibrahimhz@gmail.com>
2023-09-18 14:41:39 +0530

    Routes

--- tut6/server.js
+++ tut6/server.js
@@ -1,134 +1,97 @@
-const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
 
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-class Emitter extends EventEmitter { };
-// initialize object
-const myEmitter = new Emitter();
-// add listener to the log events
-// myEmitter.on('log', (msg) => logEvents(msg));
-// emit the event
-// myEmitter.emit('log', 'Log Event Emitted!');
-
-// add listener to the log events
-myEmitter.on('log', (msg, filename) => logEvents(msg, filename));
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 
 const PORT = process.env.PORT || 3500;
 
-const serveFile = async (filePath, contentType, response) => {
-    try {
-        const rawData = await fsPromises.readFile(
-            filePath,
-            !contentType.includes('image') ? 'utf-8' : ''
-        );
+// custom middlware logger
+app.use(logger);
 
-        const data = contentType === 'application/json'
-            ? JSON.parse(rawData) : rawData
-        response.writeHead(
-            filePath.includes('404.html') ? 404 : 200, 
-            { 'Content-Type': contentType }
-        );
-        response.end(
-            contentType === 'application/json' ? JSON.stringify(data) : data
-        );
-    } catch (err) {
-        console.log(err);
-        myEmitter.emit('log', `${err.name} : ${err.message}`, 'errLog.txt');
-        response.statusCode = 500;
-        response.end();
-    }
-}
-
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
-
-    // let filePath;
-    // 1st method
-    // if (req.url === '/' || req.url === 'index.html') {
-    //     res.statusCode = 200;
-    //     res.setHeader('Content-Type', 'text/html');
-    //     filePath = path.join(__dirname, 'views', 'index.html');
-    //     fs.readFile(filePath, 'utf-8', (err, data) => {
-    //         res.end(data);
-    //     })
-    // }
-
-    // 2nd Method
-    // switch (req.url) {
-    //     case '/':
-    //         res.statusCode = 200;
-    //         filePath = path.join(__dirname, 'views', 'index.html');
-    //         fs.readFile(filePath, 'utf-8', (err, data) => {
-    //             res.end(data);
-    //         });
-    //         break;
-    // }
-
-    const extension = path.extname(req.url);
-    let contentType;
-    switch (extension) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.jpg':
-            contentType = 'image/jpeg';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.txt':
-            contentType = 'text/plain';
-            break;
-        default:
-            contentType = 'text/html';
-    }
-
-    let filePath =
-        contentType === 'text/html' && req.url === '/'
-            ? path.join(__dirname, 'views', 'index.html')
-            : contentType === 'text/html' && req.url.slice(-1) === '/'
-                ? path.join(__dirname, 'views', req.url, 'index.html')
-                : contentType === 'text/html'
-                    ? path.join(__dirname, 'views', req.url)
-                    : path.join(__dirname, req.url);
-
-    if (!extension && req.url.slice(-1) !== '/') {
-        filePath += '.html';
-    }
-
-    const fileExists = fs.existsSync(filePath);
-
-    if (fileExists) {
-        // serve the file
-        serveFile(filePath, contentType, res);
-
-    } else {
-        // 404
-        // 301
-        switch (path.parse(filePath).base) {
-            case 'old-page.html':
-                res.writeHead(301, { 'Location': '/new-page.html' });
-                res.end();
-                break;
-            case 'www-page.html':
-                res.writeHead(301, { 'Location': '/' });
-                res.end();
-                break;
-            default:
-                // serve a 404 response
-                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+// middlware for cors cross origin resource sharing
+const whiteList = ['https://ibrahimhz.com', 'https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        console.log(origin);
+        if (whiteList.indexOf(origin) !== -1 || !origin) {
+            console.log('in the whitelist');
+            callback(null, true);
+        } else {
+            console.log('not in the whitelist')
+            callback(new Error('not allowed by CORS'));
         }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+// built-in middlewares to handle urlencoded data
+// in other words, form data;
+// 'content-type' : application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middlware for json
+app.use(express.json());
+
+// built-in middlware for serve static file
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
+
+// routes
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
+
+// Chaining Routes Concept
+
+// // Route handlers
+// app.get('/hello(.html)?', (req, res, next) => {
+//     console.log('attempted to load hello.html');
+//     next();
+// }, (req, res, next) => {
+//     res.send("hello world");
+//     next();
+// }, (req, res) => {
+//     console.log("this is second");
+// });
+
+
+// chaining route handlers
+// const one = (req, res, next) => {
+//     console.log('one');
+//     next();
+// }
+// const two = (req, res, next) => {
+//     console.log('two');
+//     next();
+// }
+// const three = (req, res) => {
+//     console.log('three');
+//     res.send('Finished');
+// }
+// app.get('/chain(.html)?', [one, two, three]);
+
+// app.use('/')
+
+// app.get('/*', (req, res) => {
+//     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// });
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    }
+    else if (req.accepts('json')) { 
+        res.json({ error: "404 not found" });
+    }else{
+        res.type('txt').send("404 not found");
     }
 });
 
-server.listen(PORT, () => console.log(`Server running on port : ${PORT}`));
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port : ${PORT}`));
